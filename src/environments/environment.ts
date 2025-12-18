9ebb83e9647f6e998be506381c7733e2a639e440
Bryan Guano <bryan.guano@epn.edu.ec>
2025-12-17 19:07:29 -0500

    feat: update environment configuration with API URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: ''
+    apiAuthUrl: 'http://127.0.0.1:4010',
+    apiAuditoriaUrl: 'http://127.0.0.1:4011',
+    apiDenunciasUrl: 'http://127.0.0.1:4012',
+    apiEvidenciasUrl: 'http://127.0.0.1:4013',
+    apiNotificacionesUrl: ' http://127.0.0.1:4014'
 };
