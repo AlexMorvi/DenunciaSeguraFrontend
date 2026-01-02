8f095860cafbccb85ace1a24f01fcc9404a1272b
Bryan Guano <bryan.guano@epn.edu.ec>
2026-01-02 08:59:39 -0500

    feat: implement denuncia page with layout and role-based action components

--- src/app/features/denuncia/views/staff/denuncia.page.ts
+++ src/app/features/denuncia/views/staff/denuncia.page.ts
@@ -1,11 +1,33 @@
+import { Component, signal, inject, OnInit, computed } from '@angular/core';
+import { DenunciaStaffViewResponse } from '@/core/api/denuncias/models/denuncia-staff-view-response';
+import { DenunciaLayoutComponent } from '@/core/layout/denuncia-layout/denuncia-layout.component';
+import { DenunciaDetailsComponent } from '../../ui/denuncia-details/denuncia-details.component';
+import { ActionsSupervisorComponent } from '../../ui/action-supervisor/actions-supervisor.component';
+import { ActionsOperadorComponent } from '../../ui/action-operador/actions-operador.component';
+import { ActionsJefeComponent } from '../../ui/action-jefe/actions-jefe.component';
+import { DenunciaFacade } from '@/data/services/denuncia.facade';
+import { AuthFacade } from '@/data/services/auth.facade';
+import { ROLES } from '@/shared/constants/roles.const';
+
+@Component({
+    selector: 'app-denuncia-page',
+    standalone: true,
+    imports: [DenunciaLayoutComponent, DenunciaDetailsComponent, ActionsSupervisorComponent, ActionsJefeComponent, ActionsOperadorComponent],
+    templateUrl: './denuncia.page.html',
+})
+export class DenunciaPageComponent {
+    denunciaService = inject(DenunciaFacade);
+    authService = inject(AuthFacade);
+
     // TODO: utilizar este signal para la denuncia actual
     // denuncia = this.denunciaService.currentDenuncia;
     currentUser = this.authService.currentUser;
+
     // TODO: Lo comentado es la implementación final, por ahora devuelve siempre true
     isJefe = computed(() => {
         // const rol = this.authService.currentUser()?.rol;
         // return [ROLES.JEFE_INTERNO, ROLES.JEFE_EXTERNO].includes(rol as any);
-        return true;
+        return false;
     });
 
     isSupervisor = computed(() => {
@@ -17,7 +39,7 @@
     isOperador = computed(() => {
         // const rol = this.authService.currentUser()?.rol;
         // return [ROLES.OPERADOR_INTERNO, ROLES.OPERADOR_EXTERNO].includes(rol as any);
-        return false;
+        return true;
     });
 
     // TODO: eliminar este signal temporal y utilizar el de la fachada
@@ -39,3 +61,4 @@
         ],
         ubicacion: { lat: -0.18, lng: -78.4, address: 'Av. Siempre Viva 123' }
     });
+}
