f21d4b5fbcd9cd4d4209688a3b8cfc1cf7c295f6
Bryan Guano <bryan.guano@epn.edu.ec>
2026-01-02 08:59:52 -0500

    refactor: rename APP_ROLES to ROLES and reorganize role constants

--- src/app/shared/constants/roles.const.ts
+++ src/app/shared/constants/roles.const.ts
@@ -1,6 +1,12 @@
 import { RolEnum } from '@/core/api/auth/models/rol-enum';
 
-export const APP_ROLES = {
+export const ROLES = {
     ADMIN: 'ADMIN_PLATAFORMA' as RolEnum,
-    CIUDADANO: 'CIUDADANO' as RolEnum,
+    SUPERVISOR: 'SUPERVISOR_DENUNCIAS' as RolEnum,
+    JEFE_INTERNO: 'JEFE_INTERNO' as RolEnum,
+    JEFE_EXTERNO: 'JEFE_EXTERNO' as RolEnum,
+    OPERADOR_INTERNO: 'OPERADOR_INTERNO' as RolEnum,
+    OPERADOR_EXTERNO: 'OPERADOR_EXTERNO' as RolEnum,
+    AUDITOR: 'AUDITOR' as RolEnum,
+    CIUDADANO: 'CIUDADANO' as RolEnum
 } as const;
