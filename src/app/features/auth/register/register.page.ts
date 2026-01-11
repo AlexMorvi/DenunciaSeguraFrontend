404ff694632b3f49a1529685c9509b36ea0f63e7
Bryan Guano <bryan.guano@epn.edu.ec>
2026-01-11 08:31:51 -0500

    feat: add alert message handling and improve form structure in registration page

--- src/app/features/auth/register/register.page.ts
+++ src/app/features/auth/register/register.page.ts
@@ -1,23 +1,43 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import {
-    FormBuilder,
-    ReactiveFormsModule,
-    Validators
-} from '@angular/forms';
-
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputComponent } from '@/shared/ui/input/input.component';
 import { SubmitButtonComponent } from '@/shared/ui/submit-button/submit-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IconDefinition, faUserPlus, faUser, faEnvelope, faLock, faCheckCircle, faExclamationCircle, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+    IconDefinition,
+    faUserPlus,
+    faUser,
+    faEnvelope,
+    faLock,
+    faCheckCircle,
+    faExclamationCircle,
+    faInfoCircle,
+    faAddressCard
+} from '@fortawesome/free-solid-svg-icons';
 import { UiStyleDirective } from '@/shared/style/ui-styles.directive';
+import { AuthFacade } from '@/data/services/auth.facade';
+import { RegistroCiudadanoRequest } from '@/core/api/auth/models';
+import { InputErrorComponent } from '@/shared/ui/input-error/input-error.component';
+import { ToastService } from '@/core/service/toast/toast.service';
+import { numericValidator } from '@/shared/validators/numeric.validator';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [ReactiveFormsModule, RouterLink, InputComponent, SubmitButtonComponent, FontAwesomeModule, UiStyleDirective],
+    imports: [
+        ReactiveFormsModule,
+        RouterLink,
+        InputComponent,
+        SubmitButtonComponent,
+        FontAwesomeModule,
+        UiStyleDirective,
+        ReactiveFormsModule,
+    ],
     templateUrl: './register.page.html',
-    styleUrls: ['./register.page.scss']
+    styles: [`
+        @use '@/styles/_auth.scss';`],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent {
     protected readonly faUserPlus: IconDefinition = faUserPlus;
@@ -27,36 +47,54 @@ export class RegisterComponent {
     protected readonly faCheckCircle: IconDefinition = faCheckCircle;
     protected readonly faExclamationCircle: IconDefinition = faExclamationCircle;
     protected readonly faInfoCircle: IconDefinition = faInfoCircle;
+    protected readonly faAddressCard: IconDefinition = faAddressCard;
 
     private readonly fb = inject(FormBuilder);
     private readonly router = inject(Router);
+    private readonly authFacade = inject(AuthFacade);
+    private toast = inject(ToastService);
 
-    // message = signal<string | null>(null);
+    message = signal<string | null>(null);
     messageClass = signal<'success' | 'error' | 'info' | null>(null);
     submitting = signal(false);
 
-    registerForm = this.fb.nonNullable.group({
+    form = this.fb.nonNullable.group({
         nombre: ['', [Validators.required, Validators.minLength(2)]],
+        cedula: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), numericValidator()]],
         email: ['', [Validators.required, Validators.email]],
         password: ['', [Validators.required, Validators.minLength(8)]]
     });
 
     get isFormValid(): boolean {
-        return this.registerForm.valid && this.registerForm.dirty;
+        return this.form.valid;
     }
 
-    onSubmit(): void {
-        if (this.registerForm.invalid || this.submitting()) {
-            this.registerForm.markAllAsTouched();
+
+    async onSubmit(): Promise<void> {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
             return;
         }
 
         this.submitting.set(true);
+        this.message.set(null);
+        this.messageClass.set(null);
 
-        const formData = this.registerForm.getRawValue();
+        const request: RegistroCiudadanoRequest = this.form.getRawValue();
+
+        try {
+            await this.authFacade.registerCitizen(request);
+
+            this.message.set('¡Registro exitoso! Serás redirigido para iniciar sesión.');
+            this.messageClass.set('success');
+            this.toast.showSuccess('¡Registro exitoso!', 'Por favor inicia sesión con tus credenciales.');
+
+            this.form.reset();
+            this.router.navigate(['/auth/login']);
+        } catch (error) {
+
+        } finally {
+            this.submitting.set(false);
+        }
     }
-
-    get nombreControl() { return this.registerForm.controls.nombre; }
-    get emailControl() { return this.registerForm.controls.email; }
-    get passwordControl() { return this.registerForm.controls.password; }
 }
