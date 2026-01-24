00254f30e4d29139457c69f9f75472ec49812d27
Bryan Guano <bryan.guano@epn.edu.ec>
2026-01-24 11:01:38 -0500

    fix: correct typo in apiUrl in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'hhttps://gateway.orangestone-4ddca4b7.eastus2.azurecontainerapps.io',
+    apiUrl: 'https://gateway.orangestone-4ddca4b7.eastus2.azurecontainerapps.io',
     authIssuer: 'https://gateway.orangestone-4ddca4b7.eastus2.azurecontainerapps.io/auth',
     requireHttps: true,
     showDebugInformation: false
