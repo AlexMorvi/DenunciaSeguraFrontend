29bd2c28f340f8036a188ae7031ddfab2e9f9280
Bryan Guano <bryan.guano@epn.edu.ec>
2026-01-27 18:34:15 -0500

    feat: historial 2

--- src/app/features/denuncia/ui/denuncia-history/denuncia-history.component.ts
+++ src/app/features/denuncia/ui/denuncia-history/denuncia-history.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { DenunciaEstadoHistorialResponse } from '@/core/api/denuncias/models';
 import { UiStyleDirective } from '@/shared/style/ui-styles.directive';
 
 @Component({
     selector: 'app-denuncia-history',
     standalone: true,
-    imports: [CommonModule, UiStyleDirective],
+    imports: [CommonModule, UiStyleDirective, DatePipe],
     templateUrl: './denuncia-history.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
