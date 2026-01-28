3b0628afa4d23ab4239a511efad567114b0f5434
Bryan Guano <bryan.guano@epn.edu.ec>
2026-01-28 13:57:34 -0500

    feat

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,27 +1,9 @@
-// import { inject } from '@angular/core';
-// import { CanMatchFn, Router } from '@angular/router';
-// import { AuthFacade } from '@/data/services/auth.facade';
-
-// export const authGuard: CanMatchFn = (_route, _segments) => {
-//     const authFacade = inject(AuthFacade);
-//     const router = inject(Router);
-
-//     const isLogged = !!authFacade.currentUser();
-
-//     if (isLogged) {
-//         return true;
-//     }
-
-//     return router.createUrlTree(['/login']);
-// };
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router'; // Usamos CanActivateFn, es más estándar para seguridad
-import { OAuthService } from 'angular-oauth2-oidc';
 import { AuthFacade } from '@/data/services/auth.facade';
 import { UsuariosFacade } from '@/data/services/usuarios.facade';
 
 export const authGuard: CanActivateFn = async (_route, _state) => {
-    const oauthService = inject(OAuthService);
     const authFacade = inject(AuthFacade);
     const usuariosFacade = inject(UsuariosFacade);
     const router = inject(Router);
